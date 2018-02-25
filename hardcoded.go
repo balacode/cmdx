b856e281d6cf3fd9c0ce34bda0fcebd40238ea13
balarabe@protonmail.com <balarabe@protonmail.com>
2018-02-25 01:35:11 +0100

    Add hardcoded.go with placeholder paths

---
+++ hardcoded.go
@@ -0,0 +1,38 @@
+// -----------------------------------------------------------------------------
+// (c) balarabe@protonmail.com                                    License: GPLv3
+// :v: 2018-02-25 01:32:48 7324F8                            [cmdx/hardcoded.go]
+// -----------------------------------------------------------------------------
+
+package main
+
+// TODO: all the paths in this file will be written in config files later
+
+var hardcodedAutologFiles = []string{
+	`X:\path\autotime.log`,
+}
+
+var hardcodedManualLogFiles = []string{
+	`X:\path\timelog.txt`,
+}
+
+const hardcodedDefaultLibPathOnLinux = `/x/path`
+
+const hardcodedDefaultLibPathOnWindows = `X:\path`
+
+const hardcodedRootPath = `X:\path`
+
+var hardcodedTimeLogPaths = []string{
+	`x:\path`,
+}
+
+var hardcodedIgnoreFilenamesWith = []string{
+	".log",
+	".tmp",
+	"tmp.",
+}
+
+const hardcodedWordListFile = `X:\path`
+
+var hardcodedReplaceManyPath = `X:\path\ReplaceMany`
+
+//end
